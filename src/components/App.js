c2b4b163f056ac06054da79cd5631ab57017f3ef
spongeboom <zlzlwl12@naver.com>
2019-05-28 00:32:35 +0900

    React Text File Upload & Move to Detail Page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Route} from 'react-router-dom';
 import Home from './Home';
 import Texts from './Texts';
 import Words from './Words';
+import Detail from './Detail';
 
 
 class App extends React.Component {
@@ -15,6 +16,7 @@ class App extends React.Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/texts" component={Texts} />
             <Route exact path="/words" component={Words} />
+            <Route exact path="/detail/:textID" component={Detail} />
           </div>
         </AppShell>
       </Router>
